64bc508b2d6616b1893384c741777879c96abc58
estebanCastaneira <estebanc_b@hotmail.com>
2023-06-06 15:30:42 -0300

    se agrega cors

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ require("dotenv").config();
 const path = require("path");
 const methodOverride = require("method-override");
 const express = require("express");
-
+const cors = require("cors");
 const routes = require("./routes");
 
 const APP_PORT = process.env.APP_PORT || 3000;
 const app = express();
 
+app.use(cors());
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
