5048c2cb3d6c309f13c5d050736782523cd630c4
andres-mps <andresmps@gmail.com>
2023-06-09 02:10:14 -0300

    following/er

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const authRoutes = require("./authRoutes");
-// const userFollowsRoutes = require("./userFollowsRoutes");
+const userFollowsRoutes = require("./userFollowsRoutes");
 const userRoutes = require("./userRoutes");
 const tweetRoutes = require("./tweetRoutes");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
@@ -7,20 +7,10 @@ const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 // const privateRoutes = require("./privateRoutes");
 
 module.exports = (app) => {
-  /**
-   * Notar que si el sitio está en español, tiene sentido que las URLs que se
-   * ven en la barra de direcciiones del navegador también lo estén. No así los
-   * nombres de variables, funciones, etc, que siempre se recomienda que estén
-   * en inglés.
-   */
-
   app.use("/", authRoutes);
   app.use("/users", userRoutes);
   app.use("/tweets", tweetRoutes);
+  app.use("/", userFollowsRoutes);
 
   //   app.use(ensureAuthenticated);
-
-  //   app.use("/", userFollowsRoutes); // para ver /following /followers
 };
-
-// PD: Recordar que es muy importante el orden en que se definen las rutas
